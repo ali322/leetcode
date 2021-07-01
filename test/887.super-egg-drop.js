1c96f4cc7d0d4c4718ab8f4090c6f2aba1162fc2
chenli13 <chenli13@asiainfo.com>
2021-07-01 17:06:31 +0800

    fix test

--- test/887.super-egg-drop.js
+++ test/887.super-egg-drop.js
@@ -2,6 +2,6 @@ const test = require('ava')
 const solution = require('../solution/887.super-egg-drop')
 
 test('case 1', t => {
-  const result = solution(4, 50)
-  t.is(result, 19)
+  const result = solution(2, 6)
+  t.is(result, 3)
 })
